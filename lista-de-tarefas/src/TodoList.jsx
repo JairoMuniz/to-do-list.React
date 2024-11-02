74bb80a6e0d9ac45607ff633118f313ad4fd62d6
Jairo <jairomunier@gmail.com>
2024-11-02 16:52:36 -0300

    remove unnecessary React import syntax in TodoList component

--- lista-de-tarefas/src/TodoList.jsx
+++ lista-de-tarefas/src/TodoList.jsx
@@ -1,5 +1,5 @@
 import './TodoList.css';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Icone from './assets/listaicon.png';
 
 function TodoList() {
